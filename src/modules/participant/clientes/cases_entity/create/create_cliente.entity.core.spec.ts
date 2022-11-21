861916d5062b2b1674907b9fc6372ebb68c91c86
rzjprogramador <rzjprogramador@gmail.com>
2022-11-21 11:21:15 -0300

    test: iniciar como desativado objetos da entidade cliente

--- src/modules/participant/clientes/cases_entity/create/create_cliente.entity.core.spec.ts
+++ src/modules/participant/clientes/cases_entity/create/create_cliente.entity.core.spec.ts
@@ -3,22 +3,32 @@ import { vi, describe, it, expect } from 'vitest'
 import { makerCliente } from '@src/modules/participant/clientes/makers/makers_cliente'
 import { recordsDNAParticipant } from '@src/app/use_objects_global/records_dna_participant'
 import { clienteFakeArgsRequestUnitysOne } from '@src/modules/participant/clientes/data/fakes/clienteFakeArgsRequestUnitys'
-
+import { STATUS_ACTIVE } from '@src/app/helpers/_data_global/global_data_standard/global_data_standard'
 
 describe('Create Cliente Entity >> Sucess', () => {
   const sut = makerCliente.newEntity
 
-  it(`deve criar um objeto da entidade domain Cliente com campos do sistema e request.`, async () => {
-    const argsRequest = clienteFakeArgsRequestUnitysOne
-    const records = recordsDNAParticipant
-    const input = { ...argsRequest, records }
+  const argsRequest = clienteFakeArgsRequestUnitysOne
+  const records = recordsDNAParticipant
+  const input = { ...argsRequest, records }
 
+  it(`deve criar um objeto da entidade domain Cliente com campos do sistema e request.`, async () => {
     const action = await sut.create(input)
     // console.log('BY CLIENTE ENTITY >> ', action)
 
     expect(action).toHaveProperty('id')
     expect(action).toHaveProperty('token', 'one')
     expect(action).toHaveProperty('nomeCliente', 'clienteFakeArgsRequestUnitysOne')
+  })
+
+  it(`deve iniciar como "Desativado" os objetos da entidade.`, async () => {
+    const action = await sut.create(input)
+    console.log('BY CLIENTE ENTITY >> ', action)
+
+    const response = action.records.statusActive
+    const sutStatusActive = STATUS_ACTIVE.DESABLED
+
+    expect(response).toEqual(sutStatusActive)
   })
 
 })
